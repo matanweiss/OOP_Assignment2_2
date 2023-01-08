a1fc49a76c6f9342d4dd59ab13bfb19e3036ee78
Matan Weiss <matan1111@gmail.com>
2023-01-09 00:02:44 +0200

    implemented getCurrentMax() method

--- CustomExecutor.java
+++ CustomExecutor.java
@@ -15,25 +15,56 @@ public class CustomExecutor extends ThreadPoolExecutor {
     }
 
     public <T> Future<T> submit(Task<T> task) {
-        if (task == null)
+        task.setIsWaiting(true);
+        int priority = task.getType().getPriorityValue();
+        Task.counts.put(priority, Task.counts.get(priority) + 1);
+        if (task == null || task.getCallable() == null)
             throw new NullPointerException();
-        RunnableFuture<T> ftask = newTaskFor(task.getCallable());
+        RunnableFuture<T> ftask = newTaskFor(task);
         execute(ftask);
         return ftask;
     }
 
+    @Override
+    public <T> Future<T> submit(Callable<T> callable) {
+        Task<T> task = Task.createTask(callable);
+        return submit(task);
+    }
+
+    public <T> Future<T> submit(Callable<T> callable, TaskType type) {
+        Task<T> task = Task.createTask(callable, type);
+        return submit(task);
+    }
+
+    public int getCurrentMax() {
+        if (0 < Task.counts.get(1))
+            return 1;
+        if (0 < Task.counts.get(2))
+            return 2;
+        if (0 < Task.counts.get(3))
+            return 3;
+        return 0;
+    }
+
+    // @Override
+    // protected void beforeExecute(Thread t, Runnable r) { }
+
+    // }
+
     public static void main(String[] args) {
         Callable<Integer> callable1 = (() -> 1 * 9);
-        Task<Integer> task1 = new Task<>(callable1);
+        Task<Integer> task1 = Task.createTask(callable1, TaskType.COMPUTATIONAL);
         CustomExecutor c = new CustomExecutor();
         Future<Integer> result = c.submit(task1);
+        System.out.println(c.getCurrentMax());
         int res = 0;
         try {
             res = result.get();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        System.out.println(res);
+        System.out.println(c.getCurrentMax());
         c.shutdown();
+        System.out.println(Task.counts);
     }
 }
--- Task.java
+++ Task.java
@@ -1,31 +1,74 @@
+import java.util.HashMap;
 import java.util.concurrent.Callable;
 
 public class Task<T> implements Callable<T>, Comparable<Task<T>> {
 
     private TaskType type;
     private Callable<T> callable;
+    private boolean isWaiting;
+    public static HashMap<Integer, Integer> counts = new HashMap<Integer, Integer>();
+    static {
+        counts.put(1, 0);
+        counts.put(2, 0);
+        counts.put(3, 0);
+    }
 
-    public Task(Callable<T> callable) {
+    private Task(Callable<T> callable) {
         this(TaskType.IO, callable);
     }
 
-    public Task(TaskType type, Callable<T> callable) {
+    private Task(TaskType type, Callable<T> callable) {
         this.type = type;
         this.callable = callable;
+        this.isWaiting = false;
+    }
+
+    public TaskType getType() {
+        return type;
+    }
+
+    public void setType(TaskType type) {
+        if (isWaiting) {
+            int oldPriority = this.type.getPriorityValue();
+            int newPriority = type.getPriorityValue();
+            counts.put(oldPriority, counts.get(oldPriority) - 1);
+            counts.put(newPriority, counts.get(newPriority) + 1);
+        }
+        this.type = type;
     }
 
     public Callable<T> getCallable() {
         return callable;
     }
 
+    public void setCallable(Callable<T> callable) {
+        this.callable = callable;
+    }
+
+    public boolean getIsWaiting() {
+        return isWaiting;
+    }
+
+    public void setIsWaiting(boolean b) {
+        isWaiting = b;
+    }
+
+    public int hashCode() {
+        return type.getPriorityValue() * callable.hashCode();
+    }
+
+    public boolean equals(Task<T> other) {
+        if (compareTo(other) == 0)
+            return true;
+        return false;
+    }
+
     @Override
-    public T call() {
-        try {
-            return callable.call();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return null;
-        }
+    public T call() throws Exception {
+        isWaiting = false;
+        int priority = type.getPriorityValue();
+        counts.put(priority, counts.get(priority) - 1);
+        return callable.call();
     }
 
     // factory method 1
@@ -34,7 +77,7 @@ public class Task<T> implements Callable<T>, Comparable<Task<T>> {
     }
 
     // factory method 2
-    public static <T> Task<T> createTask(TaskType type, Callable<T> callable) {
+    public static <T> Task<T> createTask(Callable<T> callable, TaskType type) {
         return new Task<T>(type, callable);
     }
 
